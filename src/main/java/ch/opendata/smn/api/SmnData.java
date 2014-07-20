cd0e4038ad16f419ebbd4c5b0155859613b41ab1
Marcel Stör <marcel@frightanic.com>
2014-07-20 23:29:46 +0200

    Splitter must omit empty strings.

--- src/main/java/ch/opendata/smn/api/SmnData.java
+++ src/main/java/ch/opendata/smn/api/SmnData.java
@@ -14,15 +14,15 @@ public class SmnData {
   private final Map<String, SmnRecord> smnRecords;
 
   public SmnData(String sourceData) {
-    List<String> records = Splitter.on("\n").trimResults().splitToList(sourceData);
-    smnRecords = new HashMap<>(records.size() - 3);
-    // omit first three lines
+    List<String> records = Splitter.on("\n").trimResults().omitEmptyStrings().splitToList(sourceData);
+    smnRecords = new HashMap<>(records.size() - 2);
+    // omit first three lines (the empty one was alreay removed be the Splitter, omitEmptyStrings)
     /*
      * MeteoSchweiz / MeteoSuisse / MeteoSvizzera / MeteoSwiss
      *
      * stn|time|tre200s0|sre000z0|rre150z0|dkl010z0|fu3010z0|pp0qnhs0|fu3010z1|ure200s0|prestas0|pp0qffs0
      */
-    for (int i = 3; i < records.size(); i++) {
+    for (int i = 2; i < records.size(); i++) {
       SmnRecord smnRecord = new SmnRecord(records.get(i));
       smnRecords.put(smnRecord.getCode(), smnRecord);
     }
